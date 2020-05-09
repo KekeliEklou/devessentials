c68a0d6080ad2ca0c4db3d16f4908ee680d32a57
Kekeli Eklou <keklou@atu.edu>
2020-05-09 00:03:08 +0000

    adds a line of text to default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,14 @@
 #This is my first attempt at using nano
 #This is my second attempt at using nano
 
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!<h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+Hello...this is git push test.
